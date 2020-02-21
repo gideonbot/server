ea47c433fb2c5a2d83819fb82ea1028e636975af
MBR <buildmachineubuntu@gmail.com>
2020-02-21 23:38:26 +0100

    eslint, constants, body-parser, express setup

---
+++ constants.js
@@ -0,0 +1,52 @@
+module.exports.HTTP_Codes = {
+    101: "101: Switching Protocols",
+    200: "200: OK",
+    201: "201: Created",
+    204: "204: No Content",
+    301: "301: Moved Permanently",
+    303: "303: See Other",
+    304: "304: Not Modified",
+    400: "400: Bad Request",
+    401: "401: Unauthorized",
+    403: "403: Forbidden",
+    404: "404: Not Found",
+    405: "405: Method Not Allowed",
+    406: "406: Not Acceptable",
+    409: "409: Conflict",
+    429: "429: Too Many Requests",
+    500: "500: Internal Server Error",
+    501: "501: Not Implemented",
+    502: "502: Bad Gateway",
+    503: "503: Service Unavailable"
+}
+
+module.exports.Soundtracks = {
+    the_flash: {
+        season_1: "https://open.spotify.com/album/0xiJwfDcZRQ77bsbjS9TF5",
+        season_2: "https://open.spotify.com/album/2EE7W74Rk1LDHzbYC9js2Z",
+        season_3: "https://open.spotify.com/album/0DCUAyJVmG1fNxkBaZqFLt",
+        season_4: "https://open.spotify.com/album/12WS02J0JBAVeEGj6aJ7R8",
+    },
+    arrow: {
+        season_1: "https://open.spotify.com/album/0Hsit07149488kiRI1UiaJ",
+        season_2: "https://open.spotify.com/album/234N1BIHMSxaKSVAM0JrOC",
+        season_3: "https://open.spotify.com/album/2SYmYysAbloAQkkr2TEncT",
+        season_4: "https://open.spotify.com/album/1IA3XNL6kv5zOpFG18M1au",
+        season_5: "https://open.spotify.com/album/7ivC8XKulyl3akjhiRtJjA",
+        season_6: "https://open.spotify.com/album/2sT1ks3i5yn0ZAGGvZQv92",
+    },
+    supergirl: {
+        season_1: "https://open.spotify.com/album/0jSqj8Tr0BrQIdqV4DZ9jD",
+        season_2: "https://open.spotify.com/album/7x4PDyJb3FJoYkRnF2hu2K",
+        season_3: "https://open.spotify.com/album/6zXns0k78HUh5UlNprKhvf",
+    },
+    legends_of_tomorrow: {
+        season_1: "https://open.spotify.com/album/0ceTcs3rDUEiyzshxjJFdB",
+        season_2: "https://open.spotify.com/album/4r6t4EyqVZskLi0jD9yOhU",
+        season_3: "https://open.spotify.com/album/0FPZ6xZBcwsupOaxKULGAF",
+    },
+    crossovers: {
+        crossover_1: "https://open.spotify.com/album/7vs9fu0ORNPmV2opBjLu5z",
+        crossover_2: "https://open.spotify.com/album/1jFljSQGFqtKkFNURoEOeI",
+    }
+}
