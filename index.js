4ca111af2563ec18a9710e8966f30a0315eb2f8b
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-21 15:36:40 +0100

    finish initial server setup

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require('express');
-const serveIndex = require('serve-index');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Gideon server online!');
-});
-
-app.use('/web', express.static('public'))
-app.use('/web', serveIndex('public'))
+app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Gideon server listening on port 3000!'));
