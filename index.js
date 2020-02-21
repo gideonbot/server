f62c482f1118775e9e25f94176a3f0ea41ea2bfc
adrifcastr <adrian.f.castro.mail@gmail.com>
2020-02-21 16:22:38 +0100

    use standard http port

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(8080, "0.0.0.0", () => console.log('Gideon server listening on port 8080!'));
+app.listen(80, "0.0.0.0", () => console.log('Gideon server listening on port 80!'));
