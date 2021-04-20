71ac5c9504d81d260ec031ab66be901f59fb6307
adrifcastr <adrifcastr@gmail.com>
2021-04-20 14:19:08 +0200

    improvement

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ app.post('/api/github', (req, res) => {
             Util.log('CI build passed successfully for `' + repo + '`');
     
             let path = repo == 'server' ? './' : '../' + repo;
-            exec('rm package-lock.json && git pull', {cwd: path}, error => {
+            exec('git update-index --assume-unchanged package-lock.json && git pull', {cwd: path}, error => {
                 if (error) Util.log('Error while syncing repo: ' + error);
             });
         }
